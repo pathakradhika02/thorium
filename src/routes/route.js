81f370598ceabc3697f787849d68f2d7a47a0a5e
pathakradhika02 <pathakradhika393@gmail.com>
2022-02-23 03:58:51 -0800

    msg

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,70 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+router.get('/movies', function(req,res) {
+    const value= ["Hera Pheri","Golmaal","Simba","Don"]
+    res.send(value)
+})
+
+router.get('/movies/:index',  function(req,res){
+    const arr= ["Hera Pheri","Golmaal","Simba","Don"]
+    const value= req.params.index
+    res.send(arr[value])
+})
+
+router.get('/moviez/:index',  function(req,res){
+    const arr= ["Hera Pheri","Golmaal","Simba","Don"]
+    const value= req.params.index
+    if (value>arr.length-1){
+        res.send("error, use a valid index")
+    }
+    else{
+        res.send(arr[value])
+    }
+    
+})
+
+router.get('/films', function(req,res){
+    const arr= [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+       res.send(arr)
+       
+})
+
+
+router.get('/films/:id', function(req, res) {
+    const arr= [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+    let value = req.params.id
+    for (i=0; i>arr.length; i++){
+        if (arr[i].id===value){
+            res.send(arr[i].name)
+        }
+        break
+    }
+    
 })
 
 
